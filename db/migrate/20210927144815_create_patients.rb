3612dfa7ca66c68a16af9368d370132560eb3116
Jelena <jelena.durovic@powerhrg.com>
2021-09-30 13:02:17 -0400

    done

--- db/migrate/20210927144815_create_patients.rb
+++ db/migrate/20210927144815_create_patients.rb
@@ -3,6 +3,8 @@ class CreatePatients < ActiveRecord::Migration[6.1]
     create_table :patients do |t|
       t.string :name
       t.integer :age
+
+      
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+5.times do 
+
+  Doctor.create(name: Faker::Name.name, doctor_type: "dentist")
+
+end 
+
+
+5.times do 
+  Patient.create(name: Faker::Name.name, age: rand(20..50))
+
+end 
+
+
+
+  Appointment.create(date: DateTime.new(2022, 07, 21), notes: "We are setting up for orthodonist", patient_id: rand(1..5), doctor_id: rand(1..5))
+  
+  Appointment.create(date: DateTime.new(2023, 01, 22), notes: "Everything looks healthy", patient_id: rand(1..5), doctor_id: rand(1..5))
+
+
+  Appointment.create(date: DateTime.new(2024, 03, 11), notes: "Braces are needed!!", patient_id: rand(1..5), doctor_id: rand(1..5))
+
+
+
+
+
+
